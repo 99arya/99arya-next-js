f84efb500c170d2bbe1ad84019a89f0cf3061795
Sumit Arya <sumit.arya@akqa.com>
2023-04-19 18:21:24 +0530

    A-003 - apostophes replaced with &apos;

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <Layout>
       <div className="container mx-auto p-4 mt-8">
-        <h1 className="text-3xl font-bold">Born in the 90's</h1>
+        <h1 className="text-3xl font-bold">Born in the 90&apos;s</h1>
         <h1 className="text-3xl font-bold">Raised on the streets</h1>
         <p className="mt-4">This is the homepage of my website built using Next.js 13 and Tailwind CSS.</p>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
 
         <h1 className={`${inter.className} text-2xl font-semibold`}>
-            Hello World, I'm Sumit Arya.
+            Hello World, I&apos;m Sumit Arya.
           </h1>
         <h2 className={`${inter.className} text-2xl font-semibold`}>
           I design and develop scalable industry standard websites.
